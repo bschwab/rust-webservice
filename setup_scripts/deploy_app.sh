d97f646820cb1d2ebec74a1f1f0f7a85b1621cba
Bryan Schwab <bschwab@gmail.com>
2019-05-30 18:02:11 -0700

    script that runs on EC2 instance

---
+++ setup_scripts/deploy_app.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+########################################
+# Put this on a Server
+# run chmod +x deploy_app.sh to make the script executable
+#
+# Execute this script:  ./deploy_app.sh bschwab/myapp:$TAG
+# Replace the $TAG with the actual Build Tag you want to deploy
+#
+########################################
+
+set -e
+
+DOCKER_IMAGE=$1
+CONAINER_NAME="myapp"
+
+# Check for arguments
+if [[ $# -lt 1 ]] ; then
+    echo '[ERROR] You must supply a Docker Image to pull'
+    exit 1
+fi
+
+echo "Deploying to Docker Container"
+
+#Check for running container & stop it before starting a new one
+if [ $(docker inspect -f '{{.State.Running}}' $CONAINER_NAME) = "true" ]; then
+    docker stop myapp
+fi
+
+echo "Starting Docker Image name: $DOCKER_IMAGE"
+
+docker run -d --rm=true -p 80:6000 --name $CONAINER_NAME $DOCKER_IMAGE
+
+docker ps -a
